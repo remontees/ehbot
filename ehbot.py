4eaf1b0fd2c7a4802dfc7606fe077e7641359718
remontees <remontees@free.fr>
2013-06-25 20:03:53 +0200

    Deux repositiory à la fois

--- ehbot.py
+++ ehbot.py
@@ -16,6 +16,10 @@ PROJECT = ''
 PROJECT_TITLE = ''
 PROJECT_OWNER = ''
 
+PROJECT2 = ''
+PROJECT2_TITLE = ''
+PROJECT2_OWNER = ''
+
 MUC_SEND = ''
 URL_SERVER = ''
 
@@ -49,9 +53,12 @@ html_parser = HTMLParser.HTMLParser()
 while nb_tours >= 0:
     httpServ = httplib2.Http(disable_ssl_certificate_validation=True)
     response, content = httpServ.request(URL_SERVER + '/api/v3/projects/' + PROJECT + '/repository/commits?private_token=' + CLE_API, "GET")
+    response2, content2 = httpServ.request(URL_SERVER + '/api/v3/projects/' + PROJECT2 + '/repository/commits?private_token=' + CLE_API, "GET")
     if response.status == 200:
         data = json.loads(content)
+        data2 = json.loads(content2)
         increment = 0
+        increment2 = 0
         for ligne in data:
             if (increment == 0 and last_commit != ligne['id']):
                 if nb_tours == 0:
@@ -64,6 +71,21 @@ while nb_tours >= 0:
                 cl.send(Message('MUC_SEND', message, typ='groupchat'))
                 last_commit = ligne['id']
                 increment = increment + 1
+            else:
+                break
+        
+        for ligne2 in data2:
+            if (increment2 == 0 and last_commit2 != ligne2['id']):
+                if nb_tours2 == 0:
+                    message2 = 'Dernier commit'
+                else:
+                    message2 = 'Nouveau commit'
+                
+                message2 += ' de ' + ligne2['author_name'] + ' : ' + html_parser.unescape(ligne2['title']) + ' - ' + URL_SERVER + '/' + PROJECT2_OWNER + '/' + PROJECT2_TITLE + '/commit/' + ligne2['id']
+                    
+                cl.send(Message('MUC_SEND', message2, typ='groupchat'))
+                last_commit2 = ligne2['id']
+                increment2 = increment2 + 1
             else:
                 break
         
